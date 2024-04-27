e4684d130b33fe5ad22fca87bbda7c8e10be894c
Diego Hernandez <diegofootball19@gmail.com>
2024-04-27 10:53:08 -0500

    add bfs, dfs

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,22 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    const q = [];
 
+    q.unshift(head);
+
+    while (q.length > 0) {
+        const v = q.shift() as BinaryNode<number>;
+        if (v?.value === needle) {
+            return true;
+        }
+
+        if (v.left) {
+            q.push(v?.left);
+        }
+
+        if (v.right) {
+            q.push(v?.right);
+        }
+    }
+
+    return false;
 }
