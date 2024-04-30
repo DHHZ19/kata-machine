14476fafce18834478bdc83f697de0c75b81e0fe
Diego Hernandez <diegofootball19@gmail.com>
2024-04-30 11:56:55 -0500

    add binary search tree

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,21 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+function SearchSource(
+    curr: BinaryNode<number> | null,
+    needle: number,
+): boolean {
+    if (!curr) {
+        return false;
+    }
 
+    if (curr.value === needle) {
+        return true;
+    }
+
+    if (curr.value < needle) {
+        return SearchSource(curr.right, needle);
+    }
+
+    return SearchSource(curr.left, needle);
+}
+export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+    return SearchSource(head, needle);
 }
